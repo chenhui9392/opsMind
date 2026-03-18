2ce6500ee2ec5a011eb083c3a9650c8f4aae4cba
chenhui空白的记忆 <hui.chenn@tineco.com>
2026-03-18 20:36:28 +0800

    feat:添加悬浮球功能

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ const getSystemUsername = () => {
 
 let mainWindow = null
 let tray = null
+let floatingBallWindow = null
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -27,7 +28,8 @@ function createWindow() {
   })
 
   mainWindow.loadFile('dist/index.html')
-  mainWindow.webContents.openDevTools() // 自动打开开发者工具
+  // 关闭自动打开开发者工具
+  // mainWindow.webContents.openDevTools()
 
   // 关闭窗口时最小化到托盘
   mainWindow.on('close', (event) => {
@@ -41,40 +43,76 @@ function createWindow() {
 }
 
 function createTray() {
-  // 创建托盘图标
-  const iconPath = path.join(__dirname, 'public', 'app.ico')
-  tray = new Tray(iconPath)
+  try {
+    // 创建托盘图标
+    const iconPath = path.join(__dirname, 'public', 'app.ico')
+    tray = new Tray(iconPath)
 
-  // 托盘菜单
-  const contextMenu = Menu.buildFromTemplate([
-    {
-      label: '显示窗口',
-      click: () => {
+    // 托盘菜单
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: '显示窗口',
+        click: () => {
+          mainWindow.show()
+        }
+      },
+      {
+        label: '退出',
+        click: () => {
+          app.quitting = true
+          app.quit()
+        }
+      }
+    ])
+
+    // 设置托盘图标提示
+    tray.setToolTip('运维智能助手')
+    // 设置托盘菜单
+    tray.setContextMenu(contextMenu)
+
+    // 点击托盘图标显示/隐藏窗口
+    tray.on('click', () => {
+      if (mainWindow.isVisible()) {
+        mainWindow.hide()
+      } else {
         mainWindow.show()
       }
-    },
-    {
-      label: '退出',
-      click: () => {
-        app.quitting = true
-        app.quit()
+    })
+  } catch (error) {
+    console.error('Error creating tray:', error)
+  }
+}
+
+function createFloatingBall() {
+  try {
+    // 创建悬浮球窗口
+    floatingBallWindow = new BrowserWindow({
+      width: 100,
+      height: 100,
+      frame: false,
+      transparent: true,
+      alwaysOnTop: true,
+      resizable: false,
+      skipTaskbar: true,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false
       }
-    }
-  ])
+    })
 
-  // 设置托盘图标提示
-  tray.setToolTip('运维智能助手')
-  // 设置托盘菜单
-  tray.setContextMenu(contextMenu)
+    // 加载悬浮球HTML
+    floatingBallWindow.loadFile(path.join(__dirname, 'public', 'floating-ball.html'))
 
-  // 点击托盘图标显示/隐藏窗口
-  tray.on('click', () => {
-    if (mainWindow.isVisible()) {
-      mainWindow.hide()
-    } else {
-      mainWindow.show()
-    }
-  })
+    // 隐藏默认菜单
+    floatingBallWindow.setMenu(null)
+
+    // 初始位置：屏幕右下角
+    const { screen } = require('electron')
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize
+    floatingBallWindow.setPosition(width - 100, height - 200)
+  } catch (error) {
+    console.error('Error creating floating ball:', error)
+  }
 }
 
 // 处理渲染进程的请求
@@ -87,11 +125,30 @@ ipcMain.on('getSystemUsernameSync', (event) => {
   event.returnValue = getSystemUsername()
 })
 
+// 处理悬浮球相关IPC事件
+ipcMain.handle('getWindowPosition', (event) => {
+  const position = floatingBallWindow.getPosition()
+  return { x: position[0], y: position[1] }
+})
+
+ipcMain.on('updateWindowPosition', (event, pos) => {
+  floatingBallWindow.setPosition(pos.x, pos.y)
+})
+
+ipcMain.on('toggleMainWindow', () => {
+  if (mainWindow.isVisible()) {
+    mainWindow.hide()
+  } else {
+    mainWindow.show()
+  }
+})
+
 app.whenReady().then(() => {
   console.log('System username:', getSystemUsername())
   createWindow()
   createMenu()
   createTray()
+  createFloatingBall()
 
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
