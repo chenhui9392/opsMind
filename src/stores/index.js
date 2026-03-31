9bfb3afcd7cb3375ca39613f2d6147a822d8ddd8
chenhui空白的记忆 <hui.chenn@tineco.com>
2026-03-31 13:21:57 +0800

    feat:添加pinia状态管理

---
+++ src/stores/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: hui.chenn
+ * @Description: 
+ * @Date: 2026-03-31 11:30:43
+ * @LastEditTime: 2026-03-31 11:30:43
+ * @LastEditors: hui.chenn
+ */
+import { createPinia } from 'pinia'
+
+const pinia = createPinia()
+
+export default pinia
